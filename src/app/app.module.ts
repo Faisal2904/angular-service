2dc9be93471dae8d11f51b8fa4072b65625336b2
Faisal2904 <ahmad.irfan94@gmail.com>
2019-04-30 15:32:33 +0530

    4 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-//import { HttpModule } from '@angular/http';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ActiveUsersComponent } from './active-users/active-users.component';
 import { InactiveUsersComponent } from './inactive-users/inactive-users.component';
+ import { UserService } from './app.module';
 
 @NgModule({
+
   declarations: [
     AppComponent,
     ActiveUsersComponent,
     InactiveUsersComponent
   ],
+  
   imports: [
     BrowserModule,
     FormsModule,
-   // HttpModule
+    HttpModule
   ],
-  providers: [],
+   providers: [UserService],
+
+ 
   bootstrap: [AppComponent]
+  
 })
 export class AppModule { }
---
+++ src/app/user.service.ts
@@ -0,0 +1,19 @@
+export class UserService{
+activeUsers = ['Max', 'Anna'];
+inactiveUsers = ['Chris', 'Manu'];
+
+
+  onSetToInactive(id: number) {
+    this.inactiveUsers.push(this.activeUsers[id]);
+    this.activeUsers.splice(id, 1);
+  }
+  onSetToActive(id: number) {
+    this.activeUsers.push(this.inactiveUsers[id]);
+    this.inactiveUsers.splice(id, 1);
+    console.log("active users"+this.activeUsers)
+      console.log("inactive users"+this.inactiveUsers)
+  }
+
+
+
+}
